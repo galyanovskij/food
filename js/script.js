80b6638a33631630d9573249b20cc1b6b12b3ef6
Oleg <oleggalyanovskij@gmail.com>
2023-12-19 01:15:36 +0200

    start timer deadline setClock

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 "use strict"
 window.addEventListener('DOMContentLoaded', () => {
+  // Tabs
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
@@ -38,14 +39,72 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-
   });
+      // Timer
+    const deadline = '2023-12-30';
+    // функція, що визначає різницю між дедлайном і актуальною датою
+    function getTimeRemaining(endtime) {
+      let days, hours, minutes, seconds;
+      const t = Date.parse(endtime) - Date.parse(new Date());
+      // якщо дата пройшла - виведемо "00"
+      if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+      // можемо сховати бок з таймером
+        // const delTimer = document.querySelector('.promotion__timer');
+        //       delTimer.classList.add('hide');
+      } else {
+            days = Math.floor(t / (1000 * 60 * 60 *24));
+            hours = Math.floor((t / (1000 * 60 *60) % 24));
+            minutes = Math.floor((t/1000/60) % 60);
+            seconds = Math.floor((t/1000) % 60);
+      }
+          return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+          }; 
+    }
 
-
-
-
-
-
+    // функція помічниз, для підстановки "0" перед одинарними цифрами
+    function setZero(num) {
+      if (num >=0 && num < 10) {
+        return `0${num}`;
+      } else {
+        return num;
+      }
+    }
+    // функція, що встановлює часи на сторінку
+    function setClock(selector, endtime) {
+      const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            // запускаємо функцію updateClock кожну секунду
+            timeInterval = setInterval(updateClock, 1000);
+      // функція для оновлення таймера
+        //ініціюємо функцію, щоб не було зависання даних лічильника з верстки
+        updateClock();
+      function updateClock() {
+        const t = getTimeRemaining(endtime);
+        // записуємо дані в код html
+        days.innerHTML = setZero(t.days);
+        hours.innerHTML = setZero(t.hours);
+        minutes.innerHTML = setZero(t.minutes);
+        seconds.innerHTML = setZero(t.seconds);
+        // функціонал зупинки таймера
+        if (t.total <= 0) {
+          clearInterval(timeInterval);
+        }
+      }
+    }
+    //запуск таймера
+    setClock('.timer', deadline);
 
 
 
