128b031ebb8feee468bb0f65d46ed0968abd60de
Oleg <oleggalyanovskij@gmail.com>
2024-01-04 16:18:32 +0200

    slider 2 metod slide plus dots indicators

--- js/script.js
+++ js/script.js
@@ -302,46 +302,186 @@ tabsParent.addEventListener('click', function(event) {
   // Slider
 
   const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
-        current = document.querySelector('#current');
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width; // знаходимо значення ширини блока слайдера
   let slideIndex = 1;
-  showSlides(slideIndex);
-  // працюємо з таблом кількості слайдів
+  let offset = 0; //параметр зміщеннян слайдера
+
   if (slides.length < 10) {
-    total.textContent = `0${slides.length}`;
-  } else {
-    total.textContent = slides.length;
-  }
-  function showSlides (n) {
-    if (n > slides.length){
-      slideIndex = 1;
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+      } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+      }
+
+  slidesField.style.width = 100 * slides.length + '%'; // ширина всіх фото
+  slidesField.style.display = 'flex' ;
+  slidesField.style.transition = '0.5s all';
+
+  slidesWrapper.style.overflow = 'hidden';
+  // робимо всі слайди однакової ширини
+  slides.forEach(slide =>{
+    slide.style.width = width;
+  });
+
+  slider.style.position = 'relative';
+// створюємо індикатори стайдів
+  const indicators = document.createElement('ol'), //створюємо список
+        dots = []; // масив для індикаторів
+  indicators.classList.add('carousel-indicators');
+      indicators.style.cssText = ` 
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+        `;  
+// вставляємо індикатори в низ слайдера
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i +  1);
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+    `;
+    if (i == 0) {
+        dot.style.opacity = 1;
     }
-    if (n < 1) {
-      slideIndex = slides.length;
+    indicators.append(dot);
+    dots.push(dot); // записуємо в масив dots 
+  }
+
+
+  next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2)*(slides.length - 1)){ // перетворюємо 500px в 500
+        offset = 0;
+    } else {
+        offset += +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == slides.length) {
+        slideIndex = 1;
+    } else {
+        slideIndex++;
     }
 
-    slides.forEach(item => item.style.display = 'none');
-    slides[slideIndex - 1].style.display = '';
-      // показуємо номер актуального слайду
-  if (slides.length < 10) {
-    current.textContent = `0${slideIndex}`;
-  } else {
-    current.textContent = slideIndex;
-  }
-  }
-  // лічильник слайдерів
-  function plusSliders(n){
-    showSlides(slideIndex += n);
-  }
- // призначаємо обробники подій на кнопри клайдера
+    if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+
+    dots.forEach(dot => dot.style.opacity = '.5'); // переключаємо прозорість у індикаторів
+    dots[slideIndex -1].style.opacity = 1;
+
+  });
+
   prev.addEventListener('click', () => {
-    plusSliders(-1);
+    if (offset == 0){ 
+        offset = +width.slice(0, width.length - 2)*(slides.length - 1);
+    } else {
+        offset -= +width.slice(0, width.length - 2);
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slideIndex == 1 ) {
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+
+    if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+    } else {
+        current.textContent = slideIndex;
+    }
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex -1].style.opacity = 1;
   });
-  next.addEventListener('click', () => {
-    plusSliders(1);
-  });
+  // управління слайдером через індикатори
+  dots.forEach(dot => {
+    dot.addEventListener('click', (e) =>{
+        const slideTo = e.target.getAttribute('data-slide-to');
+        slideIndex = slideTo;
+        offset = +width.slice(0, width.length - 2)*(slideTo - 1);
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+            if (slides.length < 10) {
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex -1].style.opacity = 1;
+    });
+  });   
+ 
+
+
+
+
+// простий варіант слайдера з show/hide
+//   showSlides(slideIndex);
+//   // працюємо з таблом кількості слайдів
+//   if (slides.length < 10) {
+//     total.textContent = `0${slides.length}`;
+//   } else {
+//     total.textContent = slides.length;
+//   }
+//   function showSlides (n) {
+//     if (n > slides.length){
+//       slideIndex = 1;
+//     }
+//     if (n < 1) {
+//       slideIndex = slides.length;
+//     }
+
+//     slides.forEach(item => item.style.display = 'none');
+//     slides[slideIndex - 1].style.display = '';
+//       // показуємо номер актуального слайду
+//   if (slides.length < 10) {
+//     current.textContent = `0${slideIndex}`;
+//   } else {
+//     current.textContent = slideIndex;
+//   }
+//   }
+//   // лічильник слайдерів
+//   function plusSliders(n){
+//     showSlides(slideIndex += n);
+//   }
+//  // призначаємо обробники подій на кнопри клайдера
+//   prev.addEventListener('click', () => {
+//     plusSliders(-1);
+//   });   
+//   next.addEventListener('click', () => {
+//     plusSliders(1);
+//   });
 
 
 
