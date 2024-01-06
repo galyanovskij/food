798ea47ff057b969bb7326d5e67b2be1853dff08
Oleg <oleggalyanovskij@gmail.com>
2024-01-07 01:01:07 +0200

    calculator start

--- js/script.js
+++ js/script.js
@@ -488,10 +488,79 @@ function deleteNotDigits(str) {
 //   next.addEventListener('click', () => {
 //     plusSliders(1);
 //   });
+// CALCULATOR
+  const result = document.querySelector('.calculating__result span');
+  let sex = 'female',
+      height,
+      weight,
+      age,
+      ratio = 1.375;
+// розрахунок
 
+  function calcTotal() {
+    if (!sex | !height | !weight | !age | !ratio) {
+        result.textContent = 'рахуємо';
+        return;
+    }
 
+    if (sex == 'female') {
+        result.textContent = Math.round((447.6 + (9.2 * weight) +(3.1 * height) + (4.3 * age)) * ratio);
+    } else {
+        result.textContent = Math.round((88.36 + (13.4 * weight) +(4.8 * height) + (5.7 * age)) * ratio);
+    }
 
-
-
+  }
+  calcTotal();
+  // отримуєсмо параметри з сайту
+    function getStaticInformation (parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+        // отримаємо елементи в середині блока клькулятора
+        elements.forEach(elem =>{
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+                console.log(ratio, sex);
+                //регулюємо класи активності  забераємо у всіх і задаємо лише потрібним
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+                e.target.classList.add(activeClass);
+                calcTotal();
+            });
+        });
+        
+    }
+    // запускаємо функцію відслідковування по двом блокам калькулятора
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+    // створюємо функцію для обробки кожного імпуту
+    function getDynamicInformation(selector) {
+        // отримуємо дані імпута
+        const input = document.querySelector(selector);
+        // навішуємо обробник події на імпут
+        input.addEventListener('input', () => {
+            // перевіримо відповідність даних стрічки
+            switch(input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                break;
+                case 'age':
+                    age = +input.value;
+                break;
+            }
+            calcTotal();
+        });
+        
+    }
+    // викликаємо функцію для 3-х селекторів
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 
 });
